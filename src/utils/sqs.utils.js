0902625e048e0e0b99e26ad1f6f023275d4ef3e5
Chapa <chapa@Leandros-MacBook-Air.local>
2021-03-07 17:33:22 -0300

    fix: sqs uri

--- src/utils/sqs.utils.js
+++ src/utils/sqs.utils.js
@@ -2,7 +2,7 @@ exports.buildSqsParams = ({event, context}) => {
     const accountId = context.invokedFunctionArn.split(":")[4];
     const endpoint = process.env.ENDPOINT;
     const sqsName = process.env.SQS_NAME;
-    const queueUrl  = endpoint + accountId + sqsName;
+    const queueUrl  = `${endpoint}${accountId}/${sqsName}`;
     return {
         MessageBody: event.body,
         QueueUrl: queueUrl
